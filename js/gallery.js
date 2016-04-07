5175b8881b687ea29eaa798da766720393cb4ff6
MaxielDJ7 <mid6>
2016-04-07 17:47:23 -0400

    Commencement of GET parser
    Validation of file input missing

--- js/gallery.js
+++ js/gallery.js
@@ -202,7 +202,28 @@ $('#nextPhoto', '#prevPhoto').hover(function(){
 
 /**********  END Part 3 **********/
 
+/**********  Part 5 **********/
 
+function getQueryParams(qs) {
+     qs = qs.split("+").join(" ");
+     var params = {},
+         tokens,
+         re = /[?&]?([^=]+)=([^&]*)/g;
+     while (tokens = re.exec(qs)) {
+         params[decodeURIComponent(tokens[1])]
+             = decodeURIComponent(tokens[2]);
+}
+     return params;
+ }
+
+ var $_GET = getQueryParams(document.location.search);
+
+//if file inputted into get and valid set as gallery input
+
+//else use default
+
+
+/**********  END Part 5 **********/
 
 $(document).ready( function() {
 
